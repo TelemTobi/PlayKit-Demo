2d094dedafe45d50932f7877977e5f321c9b2680
Telem Tobi <telem.tobi@recapp.co>
2025-12-15 10:31:17 +0200

    Initial Commit

---
+++ PlayKit-Demo/PlayKit_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayKit_DemoApp.swift
+//  PlayKit-Demo
+//
+//  Created by Telem Tobi on 15/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlayKit_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
